dc52da3c89af16a58bd9542c0eabba1ee1d2b453
Syed Hammad Ahmed <44769452+hammadattari@users.noreply.github.com>
2025-07-30 12:30:27 +0500

    Update RunExamAutomation.bat

--- RunExamAutomation.bat
+++ RunExamAutomation.bat
@@ -1,15 +1,8 @@
 @echo off
 setlocal
-REM Batch file to run the portable PowerShell script with Bypass ExecutionPolicy
-
-REM Get the folder where this BAT file is located
 set SCRIPT_DIR=%~dp0
+PowerShell -NoProfile -ExecutionPolicy Bypass -File "%SCRIPT_DIR%Create-ExamFolders.ps1"
 
-echo Launching Exam Folder Automation Tool...
-echo.
-
-REM Run PowerShell with Bypass ExecutionPolicy only for this run
-powershell.exe -NoProfile -ExecutionPolicy Bypass -File "%SCRIPT_DIR%CreateExamFolders_AnyPC.ps1"
 
 echo.
 echo Script finished.
